c18fe5fa3cfe0557530b967b9725cd7b6d6e6c8d
sawpresto-d <70409268+sawpresto-d@users.noreply.github.com>
2020-10-21 08:32:09 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ $ARM_CLIENT_SECRET=$2
 $AZ_TENANT_ID=$3
 RETRIES=0
 MAX_RETRIES=3
-until [ az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $AZ_TENANT_ID --output none || $RETRIES -ge $MAX_RETRIES ]
+until [ az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $AZ_TENANT_ID --output none ]
 do 
   echo "Waiting for service principal..."
   RETRIES=$((RETRIES+1))
