e9616d9e70df9858bec05ca72e83f670c834de0c
sawpresto-d <70409268+sawpresto-d@users.noreply.github.com>
2020-10-21 08:35:16 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ $ARM_CLIENT_SECRET=$2
 $AZ_TENANT_ID=$3
 RETRIES=0
 MAX_RETRIES=3
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 until [ az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $AZ_TENANT_ID --output none ]
 do 
   echo "Waiting for service principal..."
